5b43f4faf19f6d744a368b2d7cf137d0b5541f8d
Caleb <c4leb.johnson@gmail.com>
2018-12-24 14:48:48 -0800

    all working now

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -5,8 +5,8 @@
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
-#define BAUD_Rate (9600)
-#define BAUD_Register_Value ((64UL * F_CPU)/(16UL * BAUD_Rate))
+#define BAUD_Rate 9600
+#define BAUD_Register_Value ((32UL * F_CPU)/(16UL * BAUD_Rate))
 
 #define true 1
 #define false 0
@@ -14,9 +14,8 @@
 #define ADDRESS 12
 
 
-#define PIN_CLAPPER 0x8
-#define PIN_DAMPER  0x2
-#define PIN_TXD     0x20
+#define PIN_CLAPPER 0x20
+#define PIN_DAMPER 0x8
 
 
 struct _rx
@@ -27,8 +26,12 @@ struct _rx
 
 
 void ioinit (void) {
+	// scale back periferal clock
+	CPU_CCP = 0xD8;
+	CLKCTRL.MCLKCTRLB = CLKCTRL_PDIV_12X_gc | CLKCTRL_PEN_bm;
+
 	// set io ouputs
-	PORTA.DIR = PIN_TXD | PIN_DAMPER | PIN_CLAPPER;
+	PORTA.DIR = PIN_DAMPER | PIN_CLAPPER;
 
 	// setup uart
 	USART0.BAUD = BAUD_Register_Value;
@@ -39,9 +42,9 @@ void ioinit (void) {
 	TCA0.SINGLE.INTCTRL |= TCA_SINGLE_OVF_bm;
 	TCA0.SINGLE.CTRLA |= (0x7 << 1);
 
-	TCB0.CTRLA = TCB_CLKSEL_CLKTCA_gc;
-	TCB0.CTRLB = TCB_CNTMODE_SINGLE_gc;
-	TCB0.INTCTRL = 1;
+	TCB0.CTRLA = TCB_CLKSEL_CLKDIV2_gc;
+	TCB0.CTRLB = TCB_CNTMODE_SINGLE_gc | TCB_CCMPEN_bm;
+	// TCB0.INTCTRL = 1;
 
 	// enable global interupts
 	CPU_SREG |= CPU_I_bm;
@@ -53,42 +56,31 @@ void ioinit (void) {
 ISR(TCA0_OVF_vect) {	
 	// Clear the interrupt
 	TCA0.SINGLE.INTFLAGS = 0xff;
-	// halt and restart the timer
+	// halt the timer
 	TCA0.SINGLE.CTRLA &= ~TCA_SINGLE_ENABLE_bm;
-	TCA0.SINGLE.CTRLECLR |= 0x2 << 2; 
 	// turn off the coil
-	PORTA.OUT &= ~PIN_CLAPPER;
+	PORTA.OUT &= ~PIN_DAMPER;
 }
 
 
-ISR(TCB0_INT_vect) {
-	PORTA.OUT ^= PIN_DAMPER;
-}
-
-
-// USART rx interrupt
-/*
-
-1-m-aaaaaa : mode(1),  address(6)
-
-Mode #0:
-	0-cc-vvvvv : cmd(2), value(5)
-
-		cmd 0 = Ring
-		cmd 1 = Damp
-		cmd 2 = Mortello
-		cmd 3 = Buzz 
-
-Mode #1:
-	0-pp-vvvvvv : param(2), value(6)
-
-		param 0 = min ring value
-		param 1 = max ring value
-		param 2 = buzz value
-		param 3 = latency?
-
-*/
 ISR(USART0_RXC_vect) {
+	/* === USART rx interrupt ===
+		1-m-aaaaaa : mode(1),  address(6)
+
+		Mode #0:
+			0-cc-vvvvv : cmd(2), value(5)
+				cmd 0 = Ring
+				cmd 1 = Damp
+				cmd 2 = Mortello
+				cmd 3 = Buzz 
+
+		Mode #1:
+			0-pp-vvvvvv : param(2), value(6)
+				param 0 = min ring value
+				param 1 = max ring value
+				param 2 = buzz value
+				param 3 = latency?
+	*/
 	uint8_t value = USART0.RXDATAL;
 
 	// get instruction type
@@ -96,33 +88,37 @@ ISR(USART0_RXC_vect) {
 		rx.address = value & 0x3F;
 		rx.mode = (value >> 6) & 0x1;
 	} else if (rx.address == ADDRESS) {
-		// only bother parsing commands to us
+		// only bother parsing commands addressed to us
 		if (rx.mode) {
 			// TODO: configs
 		} else {
-			if (((value >> 5) & 0x3) == 0) {
-				// Dampen Bell
-				TCB0.CTRLA |= TCB_ENABLE_bm;
-				TCB0.CCMP = 0xff;
-			} else {
-				// Ring Bell
-				PORTA.OUT |= PIN_CLAPPER;
+			if ((value >> 5) & 0x3) {
+				// dampen bell
+				PORTA.OUT |= PIN_DAMPER;
 				// start timer
-				TCA0.SINGLE.PER = (value & 0x1F) << 3;
+				TCA0.SINGLE.CTRLECLR |= 0x2 << 2; 
+				TCA0.SINGLE.PER = 0x2ff;
 				TCA0.SINGLE.CTRLA |= TCA_SINGLE_ENABLE_bm;
+			} else {
+				// turn off damper (if it's still on)
+				PORTA.OUT &= ~PIN_DAMPER;
+				// ring bell using Timer B
+				TCB0.CCMP = (value & 0x1F) << 11;
+				TCB0.CNT = 0;
+				TCB0.CTRLA |= TCB_ENABLE_bm;
 			}
 		}
 	}
-	// USART0.TXDATAL = rx;
 }
 
 
-
 int main (void) {
-	ioinit();
 	rx.address = 0;
+	ioinit();
 	while(1) {
-		_delay_ms(20);
+		// global loop of nothing
+		// everything is handled by interrupts
+		_delay_ms(10);
 	}
 	return (0);
 }
