9c557b15286c2011cb461e3a86a050d52b8a4c70
Caleb <c4leb.johnson@gmail.com>
2019-06-22 12:24:42 -0700

    added tools

---
+++ firmware/src/flash_tool.py
@@ -0,0 +1,22 @@
+import os
+
+while True:
+    print("----------------------")
+    address = str(input("Enter Address: "))
+
+    # exit loop
+    if address.lower().strip() in ["exit", "quit", "q", "stop"]:
+        break
+    else:
+        address = int(address)
+
+    # write address to the config file
+    config_file = open("my_address.h", "w")
+    config_file.write("#define MY_ADDRESS %d\n" % address)
+    config_file.close()
+
+    # compile with new address
+    os.system("make")
+
+    # flash to chip
+    os.system("./flash.sh")
---
+++ firmware/src/my_address.h
@@ -0,0 +1,1 @@
+#define MY_ADDRESS 16
